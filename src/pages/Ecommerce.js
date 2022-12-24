0a801871dfd6feab2a03b57d6068772bd2745a7a
khazarglzd <khazarguluzade@gmail.com>
2022-12-25 02:03:14 +0400

    adding Stacked component to Ecommerce page

--- src/pages/Ecommerce.js
+++ src/pages/Ecommerce.js
@@ -100,13 +100,13 @@ const Ecommerce = () => {
                   borderRadius="10px"
                 />
               </div>
-
             </div>
-
-        </div>
+            <div>
+              <Stacked width="320px" height="360px" />
+            </div>
+          </div>
+       </div>
       </div>
-
-    </div>
     </div>
   );
 };
