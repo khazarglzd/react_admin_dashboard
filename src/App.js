417a09544b5881323226be2d824700ccc93e1f14
khazarglzd <khazarguluzade@gmail.com>
2022-12-28 23:50:20 +0400

    adding ThemeSettings comp

--- src/App.js
+++ src/App.js
@@ -44,6 +44,8 @@ const App = () => {
           </div>
 
           <div>
+              <ThemeSettings />
+
               <Routes>
                 {/* dashboard  */}
                 <Route path="/" element={(<Ecommerce />)} />
